1e14a808e94046e418b83ca86550c5e7cb72607f
tuminbangkok <42670714+tuminbangkok@users.noreply.github.com>
2023-02-27 20:13:14 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.45.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
 resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "Southeast Asia"
