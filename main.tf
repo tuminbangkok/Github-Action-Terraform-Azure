bff6d76fe5a0ce7cfb50e9aa3a306943c2de8d26
tuminbangkok <attapol_siam@hotmail.com>
2023-02-27 19:06:31 +0700

    ddfd

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
+/* terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.13.0"
     }
   }
-}
+} */
 
 provider "azurerm" {
   features {
